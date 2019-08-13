3244afba5be78cd4310fbd1dd621396c15e81698
aumnaja2546 <51729168+aumnaja2546@users.noreply.github.com>
2019-08-13 17:35:35 +0700

    Update app.py

--- app.py
+++ app.py
@@ -31,7 +31,6 @@ def callback():
        sendtext(user,'ฉันไม่เข้าจายย')
     
     return '',200
-
 def sendText(user, text):
   LINE_API = 'https://api.line.me/v2/bot/message/reply'
   headers = {
